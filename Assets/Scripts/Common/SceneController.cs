adb058a20f21f49a6a85414809b63a5a3bd611c1
Mikhail Kryloff <mikhail.kryloff@varwin.com>
2021-08-30 02:58:14 +0500

    Кнопка рестарта

---
+++ Assets/Scripts/Common/SceneController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Common
+{
+    public class SceneController : MonoBehaviour
+    {
+        /// <summary>
+        /// Calls from UI button
+        /// </summary>
+        public void Reload()
+        {
+            var currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.name);
+        }
+    }
+}
